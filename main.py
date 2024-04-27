c7a5218547b278f382cc349ad3c6f6d0d1adfc6a
lewis <lewiskinyuanjue.ke@gmail.com>
2024-04-27 07:30:22 +0300

    make collision more effective and score

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Any
+
 import pygame 
 pygame.init()
 
@@ -11,6 +11,10 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('PONG')
 FPS = 60 
 VELOCITY = 10
+SCORE_FONT = pygame.font.Font(None,20)
+
+RIGHT_SCORE = 0 
+LEFT_SCORE = 0 
 
 
 class Ball:
@@ -42,11 +46,16 @@ class Ball:
         ):
           
             self.x_velocity *= -1
+            H = left_paddle.y + left_paddle.height//2 
+            y_pos= self.y 
+            D = y_pos - H 
+            max_height = left_paddle.height//2 
+            
+            self.y_velocity = (D // 20) * 10 
 
-            d= (self.y- self.radius) - (left_paddle.y + (left_paddle.height//2))+7
-            self.y_velocity = 2* d
+           
 
-            print(d)
+            
 
         if (  # Collision with right paddle
                 self.x + self.radius > right_paddle.x and
@@ -57,8 +66,32 @@ class Ball:
         ):
             
             self.x_velocity *= -1
-            d = (self.x + self.radius) - (right_paddle.y + (right_paddle.height//2))+7
-            self.y_velocity = 2* d
+
+            H = right_paddle.y + right_paddle.height//2 
+            y_pos= self.y 
+            D = y_pos - H 
+            max_height = right_paddle.height//2 
+            
+            self.y_velocity = (D // 20) * 10 
+
+    def Score(self):
+        if self.x < 0:
+            global RIGHT_SCORE
+            RIGHT_SCORE +=1 
+            self.x = WIDTH// 2 
+            self.y = HEIGHT// 2
+            self.x_velocity *= -1 
+            self.y_velocity = 0
+
+        if self.x > WIDTH:
+            global LEFT_SCORE
+            LEFT_SCORE +=1 
+            self.x = WIDTH// 2
+            self.y = HEIGHT// 2
+            self.x_velocity *= -1 
+            self.y_velocity = 0
+
+        
 
 
 
@@ -88,8 +121,8 @@ class Paddle:
 
 
    
-left_paddle = Paddle(0,WIDTH//2 - 25,10,50,WHITE)
-right_paddle = Paddle(WIDTH - 10,HEIGHT//2 - 25,10,50,WHITE)
+left_paddle = Paddle(0,WIDTH//2 - 50,10,100,WHITE)
+right_paddle = Paddle(WIDTH - 10,HEIGHT//2 - 50,10,100,WHITE)
 
 
 
@@ -98,13 +131,26 @@ def draw_border():
     i=0
     while i < 600:
         pygame.draw.rect(WIN,WHITE,(WIDTH//2 - 5,i,10,20))
+
+
         i += 30
+
+def draw_score():
+    LEFT_SCORE_TEXT=SCORE_FONT.render(f'SCORE:  {LEFT_SCORE}',0,WHITE)
+    RIGHT_SCORE_TEXT=SCORE_FONT.render(f'SCORE: {RIGHT_SCORE}',0,WHITE)
+    LEFT_SCORE_TEXT_RECT = LEFT_SCORE_TEXT.get_rect(center=(WIDTH//4,50))
+    RIGHT_SCORE_TEXT_RECT=RIGHT_SCORE_TEXT.get_rect(center=(3 * WIDTH//4,50))
+    WIN.blit(LEFT_SCORE_TEXT,LEFT_SCORE_TEXT_RECT)
+    WIN.blit(RIGHT_SCORE_TEXT,RIGHT_SCORE_TEXT_RECT)
+
 
 def draw():
     WIN.fill(BLACK)
     ball.draw()
     ball.move()
+    ball.Score()
     draw_border()
+    draw_score()
     left_paddle.draw_rect()
     right_paddle.draw_rect()
     left_paddle.move_up()
